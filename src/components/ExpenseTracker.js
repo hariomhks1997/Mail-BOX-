1896046ca9765a872813074388cb5e6103c6b598
Your Name01~ <you@example.com>
2023-08-18 17:36:34 +0530

    4 Edit User Details

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import CardHeader from "react-bootstrap/esm/CardHeader";
 import { NavLink } from "react-router-dom";
@@ -6,17 +7,18 @@ const ExpenseTracker = () => {
   
   return (
     <div>
-    <div style={{ marginTop: "6rem", display: "flex" }}>
-      <CardHeader style={{ backgroundColor: "violet", paddingBlock: "1rem" }}>
-        Welcome to Expense Tracker
-      </CardHeader>
-      
-      <CardHeader style={{ marginLeft: "70%", background: "yellow" }}>
-        <li>
-          <NavLink to="/complete">
+    <div style={{ marginTop: "6rem" }}>
+      <CardHeader style={{ backgroundColor: "violet", paddingBlock: "1rem",display:'flex',justifyContent:'space-between',borderRadius:'1rem' }}>
+        <li style={{listStyle:'none',padding:'1rem',background:'white',borderRadius:'1rem'}}>Welcome to Expense Tracker</li>
+        <li style={{listStyle:'none',background:'lightyellow',padding:'1rem',borderRadius:'1rem'}}>
+          <NavLink style={{textDecoration:'none'}} to="/complete">
             Your Profile Is Incomplete : Complete Now
           </NavLink>
         </li>
+      </CardHeader>
+      
+      <CardHeader style={{ marginLeft: "70%", background: "yellow" }}>
+        
       </CardHeader> 
       
     </div>
