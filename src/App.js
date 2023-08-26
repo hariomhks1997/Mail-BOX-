eb0e9808f6bdc92fcc2b63336a1de600fd6b797a
Your Name01~ <you@example.com>
2023-08-26 12:11:30 +0530

    18.2 Shoping page app using reducers

--- src/App.js
+++ src/App.js
@@ -1,60 +1,15 @@
-import React,{useContext, useState} from 'react';
-import Login from './components/Login';
-import Navbars from './components/Navbars';
-import { Routes,Route } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import ExpenseTracker from './components/ExpenseTracker';
-import CartContext from './Store/Cart-context';
-import ProfileComplete from './components/ProfileComplete';
-import EmailVerification from './components/EmailVerification';
-import ForgetPassword from './components/ForgetPassword';
-
-
-const App = () => {
-  const cartctx = useContext(CartContext)
-  console.log(cartctx.totalamount)
-  const [theme, settheme] = useState(true)
-  const [emailverify, setemailverify] = useState(true)
-  const [colo, setcolo] = useState('white')
-  const color=(item)=>{
-    setcolo(item)
-    console.log(item)
-      }
-  const shownhandler=()=>{
-    setemailverify(false)
-  }
-  const amount=(item)=>{
-settheme(item)
-
-  }
-  const premium=(item)=>{
-setcolo(item)
-  }
-  
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import { useSelector } from 'react-redux';
+function App() {
+  const showcart=useSelector((state)=>state.ui.cartIsVisible)
   return (
-    <div style={{background:colo}}>
-     
-     
-    <Navbars amount={theme} color={color}></Navbars>  
-    <Routes>
-    <Route exact path='/home' element={<Home></Home>}></Route>
-    <Route exact path='/about' element={<About></About>}></Route>
-    <Route exact path='/expensetracker' element={emailverify ? <EmailVerification shown={shownhandler}></EmailVerification>:<ExpenseTracker token={cartctx.token} amount={amount} premium={premium}></ExpenseTracker>}></Route>
-    
-   <Route exact path='/complete' element={<ProfileComplete  ></ProfileComplete>}></Route>
-
-    <Route exact path='/login' element={<Login></Login>}></Route>
-  {emailverify && <Route exact path='/verifyemail' element={<EmailVerification shown={shownhandler}></EmailVerification>}></Route>}
-  <Route exact path='/forget' element={<ForgetPassword></ForgetPassword>}></Route>
-    
-      
-      
-    </Routes>
-   
- 
-    </div>
-  )
+    <Layout>
+      {showcart && <Cart />}
+      <Products />
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Main } from 'react-router-dom';
-import CartProvider from './Store/CartProvider';
 import { Provider } from 'react-redux';
+import store from './store/redux';
 
-import store from './Store/redux-demo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}><CartProvider><Main><App /></Main></CartProvider></Provider>);
+root.render(<Provider store={store}><App /></Provider>);
