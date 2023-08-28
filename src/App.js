e5617fb8a11c6771733fd27ac7d792e327a463dd
Your Name01~ <you@example.com>
2023-08-28 09:45:22 +0530

    21.2 Fixing the Cart problems

--- src/App.js
+++ src/App.js
@@ -4,88 +4,25 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
-import { cartActions } from './store/cart-slice';
-import axios from 'axios';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
-//console.log(isInitial)
 
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
- // console.log(cart)
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
-
-      const response = await fetch(
-        'https://reacthariom-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart),
-        }
-      );
-     
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-    };
-    
     if (isInitial) {
       isInitial = false;
       return;
     }
-   
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
-  useEffect(() => {
-    
-    const fetch=async ()=>{
-      try{
-        const response =await axios.get( 'https://reacthariom-default-rtdb.firebaseio.com/cart.json');
-      //console.log(response.data.items)
-      dispatch(cartActions.getitem(response.data.items))
-      dispatch(cartActions.getquantity(response.data.totalQuantity))
-      }catch(err){
-
-      }
-
-    }
-    fetch();
-  
-    
-  }, [])
-  
 
   return (
     <Fragment>
